87d178b1830ed5e05a1934036bdcabb51ac99728
me <972294803@qq.com>
2020-01-12 09:42:44 +0800

    工具类

---
+++ src/test/java/com/qiushengfei/common/utils/test/FileTests.java
@@ -0,0 +1,16 @@
+package com.qiushengfei.common.utils.test;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.qiushengfei.common.utils.FileUtil;
+
+public class FileTests {
+	@Test
+	public void readTextFileToList() {
+		String fileName = "C:\\Users\\Administrator\\Desktop\\pom.xml";
+		List<String> readTextFileToList = FileUtil.readTextFileToList(fileName);
+		System.out.println(readTextFileToList);
+	}
+}
